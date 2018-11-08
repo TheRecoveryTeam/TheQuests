eab52d4e24a07e0a0f9f98ea786390c566edd0ce
Dmitry Pudov <dimapudov99@gmail.com>
2018-11-08 12:40:04 +0300

    Git clean

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,3 +1,6 @@
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
 #include <iostream>
 
 #include <bsoncxx/builder/stream/document.hpp>
@@ -7,19 +10,19 @@
 #include <mongocxx/instance.hpp>
 
 
-int main(int, char**) {
-  mongocxx::instance inst{};
-  mongocxx::client conn{mongocxx::uri{"mongodb://localhost:27017"}};
+int main(int, char **) {
+    mongocxx::instance inst{};
+    mongocxx::client conn{mongocxx::uri{"mongodb://localhost:27017"}};
 
-  bsoncxx::builder::stream::document document{};
+    bsoncxx::builder::stream::document document{};
 
-  auto collection = conn["testdb"]["user"];
-  document << "hello" << "world";
-  collection.insert_one(document.view());
-  auto cursor = collection.find({});
+    auto collection = conn["testdb"]["user"];
+    document << "hello" << "world";
+    collection.insert_one(document.view());
+    auto cursor = collection.find({});
 
-  for (auto&& doc : cursor) {
-    std::cout << bsoncxx::to_json(doc) << std::endl;
-  }
-  return 0;
+    for (auto &&doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << std::endl;
+    }
+    return 0;
 }
