b428217d89e08fe4ec64bedd2d0ce05b242ff307
Dmitry Pudov <dimapudov99@gmail.com>
2018-11-12 17:12:32 +0300

    card.get and card.list mocks

--- backend/controllers/card/CardController.cpp
+++ backend/controllers/card/CardController.cpp
@@ -13,8 +13,28 @@ void CardController::handleGet(http_request message) {
     auto path = requestPath(message);
     if (!path.empty()) {
         if (path[0] == "card" && path[1] == "get") {
+            // TODO: get model from db
+            int id = 1;
+            int questId = 1;
+            string_t title = "title";
+            string_t imagePath = "imagePath";
+            string_t description = "description";
+            string_t type = "choose";
+            auto response = json::value::object();
+            response["id"] = id;
+            response["questId"] = questId;
+            response["title"] = json::value::string(title);
+            response["imagePath"] = json::value::string(imagePath);
+            response["description"] = json::value::string(description);
+            response["type"] = json::value::string(type);
+            message.reply(status_codes::OK, response);
 
         } else if (path[0] == "card" && path[1] == "list") {
+            // TODO: get model from db
+
+            auto response = json::value::object();
+//            auto cards = json::array();
+            message.reply(status_codes::OK, response);
 
         } else {
             auto response = json::value::object();
